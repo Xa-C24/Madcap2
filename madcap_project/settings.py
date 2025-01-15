2391cdbea5bbc7162783d1e058d3420f89d8a2bf
Xa-C24 <9335@holbertonstudents.com>
2025-01-15 14:34:32 +0100

    add parametre SMTP gmail ligne 108

--- madcap_project/settings.py
+++ madcap_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import logging
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,6 +105,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Paramètres SMTP pour Gmail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'xr.piallu@gmail.com'
+EMAIL_HOST_PASSWORD = 'fzzr vsna uyfp lmoa'  # Remplacez par le mot de passe de l'adresse email
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
