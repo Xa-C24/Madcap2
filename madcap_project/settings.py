bd483d9d94a21f93add5c4b31153f517769bccdf
Xa-C24 <9335@holbertonstudents.com>
2025-02-11 13:47:10 +0100

    modif commentaire

--- madcap_project/settings.py
+++ madcap_project/settings.py
@@ -111,7 +111,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'xr.piallu@gmail.com'
-EMAIL_HOST_PASSWORD = 'fzzr vsna uyfp lmoa'  # Remplacez par le mot de passe de l'adresse email
+EMAIL_HOST_PASSWORD = 'fzzr vsna uyfp lmoa'  #  le mot de passe de l'adresse email
 
 
 logging.basicConfig(
