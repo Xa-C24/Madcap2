b6e234942f7b0a2206b0b726ebf2ef0fde34ebe0
Xa-C24 <9335@holbertonstudents.com>
2025-02-13 08:54:17 +0100

    Ajout chemin vers admin/

--- madcap_project/urls.py
+++ madcap_project/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),  # Route pour l'admin
     path('', include('madcap_app.urls')),  # Inclure les routes de l'application
